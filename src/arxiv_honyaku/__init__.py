764a96d9b106174a4f4cc72ab3723b265f84d5b3
sankakusango <sankakusango@gmail.com>
2026-05-07 19:54:00 +0900

    Add __init__

---
+++ src/arxiv_honyaku/__init__.py
@@ -0,0 +1,22 @@
+"""arxiv-honyaku."""
+from .arxiv_source import download_and_unpack
+from .config import load_config
+from .build_latex import compile_tex_trying_texlive_versions, find_main_tex
+from .source_tree import load_source_tree, resolve_source_root, save_source_tree
+from .prepare_translation import (
+    apply_irreversible,
+    apply_japanese_setup,
+    load_translation_jsonl,
+    prep_json_path,
+    prepare_from_source_tree,
+    prepare_tex_file,
+    reconstruct_from_source_tree,
+    reconstruct_translated_from_source_tree,
+    restore_from_json,
+)
+from .translate import (
+    iter_translation_chunks,
+    translate_source_tree_to_pdf,
+    translate_prep_dir,
+    translate_prep_dir_async,
+)
